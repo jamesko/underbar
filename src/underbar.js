39f43b64c95c5f82d3616416f3857bf4f944000b
James Kolonusz <jamesak@gmail.com>
2014-11-19 19:19:53 -0800

    Add code for _.reject function.

--- src/underbar.js
+++ src/underbar.js
@@ -91,6 +91,8 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var invertedTest = function(item){return !(test(item))};
+    return _.filter(collection, invertedTest);
   };
 
   // Produce a duplicate-free version of the array.
