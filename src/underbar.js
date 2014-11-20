495789b31e27e31cd7e5f7fd91efac0451b42e1b
James Kolonusz <jamesak@gmail.com>
2014-11-19 17:55:03 -0800

    Add code for _.last function.

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,8 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n == 0) return [];
+    return n === undefined ? array.slice(-1)[0] : array.slice(-n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
