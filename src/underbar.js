ed0d5a6d2c4767c11ed7d785614e20cd4abeffcf
James Kolonusz <jamesak@gmail.com>
2014-11-20 13:52:58 -0800

    Add code for _.identity function.

--- src/underbar.js
+++ src/underbar.js
@@ -7,7 +7,7 @@ var _ = {};
   // Returns whatever value is passed as the argument. This function doesn't
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
-  _.identity = function(val) {
+  _.identity = function(val) { return val;
   };
 
   /**
