e1469a6efc3fbe1d67c5865e2e0dd76cb701f39a
James Kolonusz <jamesak@gmail.com>
2014-11-20 17:41:42 -0800

    Add code for _.some function.

--- src/underbar.js
+++ src/underbar.js
@@ -212,6 +212,11 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if(arguments[1]==undefined) {iterator = _.identity};
+    if(collection.length==0) return false;
+    return !(_.every(collection, function(item){
+      return !(iterator(item))
+    }));
   };
 
 
